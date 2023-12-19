b8acad76d23fddd926e5297d70c40ea619ad1ae3
ck96548 <ck96548@gmail.com>
2023-12-20 00:17:05 +0530

    deep drive with get post patch and delete api

---
+++ src/routes/api/comments/[commentId]/+server.js
@@ -0,0 +1,30 @@
+import { json } from "@sveltejs/kit";
+import { comments } from "../../../../lib/comment";
+
+export function GET(requestEvent){
+    const {params} = requestEvent;
+    const {commentId} = params;
+    const comment = comments.find((comment)=>comment.id === parseInt(commentId))
+    return json(comment);
+}
+
+export async function PATCH(requestEvent){
+    const {params,request} = requestEvent;
+    const {commentId} = params;
+    const {text} = await request.json();
+
+    const comment = comments.find((comment)=>comment.id === parseInt(commentId))
+    comment.text = text;
+    return json(comment)
+}
+
+export async function DELETE(requestEvent){
+    const {params} = requestEvent;
+    const {commentId} = params;
+    
+
+    const deleteComment = comments.find((comment)=>comment.id === parseInt(commentId))
+    const index = comments.findIndex((comment)=>comment.id === parseInt(commentId))
+    comments.splice(index,1);
+    return json(deleteComment);
+}
